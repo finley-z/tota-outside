351e994683f1661ce3d94158112cf2a2d74d1deb
illhaveacat <1258194015gm@gmail.com>
2018-06-04 16:27:41 +0800

    config

--- tota-outside-rpc/src/main/java/com/tota/outside/rpc/service/DubboTestServiceImpl.java
+++ tota-outside-rpc/src/main/java/com/tota/outside/rpc/service/DubboTestServiceImpl.java
@@ -5,24 +5,30 @@ import com.tota.outside.rpc.api.model.ConsumeMessage;
 import com.tota.outside.rpc.api.service.DubboTestService;
 import com.tota.outside.rpc.resolver.ConsumeMsgResolver;
 import com.tota.outside.rpc.resolver.Resolver;
+import com.tota.outside.util.LogUtil;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Service
 public class DubboTestServiceImpl implements DubboTestService {
+
     @Override
     public String testDubbo() {
-        System.out.println("client is coming ****************************************************");
+        LogUtil.log(this.getClass()).info("client is coming ****************************************************");
+        LogUtil.log(this.getClass()).info("dubbo interface is running");
         return "dubbo interface is running";
     }
 
-
     public static void main(String[] args) {
+        PropertyConfigurator.configure( "log4j.properties" );
         String consumeMessage = "029511000000000100015000102461201406251040324A49D0760051000001000000000006110000000001000000000000000000000029111000000000100000000000000000000000000000000523014014444042300000014011000020000000010000000100203412122014062510403252300020140625000362201406250101273055000000000000000000000000000000000";
         Resolver cmr = new ConsumeMsgResolver();
 //        ConsumeMessage consumeMsg = (ConsumeMessage) cmr.test(consumeMessage);
 //        System.out.println(JSON.toJSONString(consumeMsg));
+        LogUtil.log(DubboTestServiceImpl.class).info("client is coming ****************************************************");
+        LogUtil.log(DubboTestServiceImpl.class).info("dubbo interface is running");
     }
 }
 
